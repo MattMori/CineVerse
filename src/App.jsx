6f0d772b410b33030c84f21080217f573036f8a0
MattMori <Mateus5310@live.com>
2023-08-04 00:41:51 -0300

    atualização visual: alteração no background do site

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/Home/Home.jsx";
 import MovieDetail from "./pages/MovieDetail/MovieDetail";
-
 function App() {
   const [searchValue, setSearchValue] = useState("");
 
   
   return (
-    <div className="App" style={{ backgroundColor: "#303030" }}>
+    <div className="App">
       <Header onSubmit={(inputValue) => setSearchValue(inputValue)} />
       <Routes>
         <Route path="/" element={<Home searchValueProp={searchValue} />} />
