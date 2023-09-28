8257dc8e339ee381e856f7522acdff4185c19955
MattMori <Mateus5310@live.com>
2023-09-27 21:06:50 -0300

    Alterações no Home e MovieCard: Foi realizado Alterações no home para que o tamanho do card fosse exibido corretamente. Realizei Alterações no MovieCard para tornar o card inteiro em um link clicavel que direciona ao movieDetails, retirei o botão de ver detalhes que direcionava e inclui data de lançamento e votos no lugar.

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,20 +1,31 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
-
+ 
 const MovieCard = ({ movie }) => {
-  
+ 
+  function formatarNumero(Flutuante) {
+    return parseFloat(Flutuante).toFixed(1);
+  }
+
+  function formatarDataBrasileira(data) {
+    const dataObjeto = new Date(data);
+    const dia = dataObjeto.getDate().toString().padStart(2, '0');
+    const mes = (dataObjeto.getMonth() + 1).toString().padStart(2, '0');
+    const ano = dataObjeto.getFullYear();
+
+    return `${dia}/${mes}/${ano}`;
+  }
   return (
+    <Link to={`/movie/${movie.id}`} className='btn-details'>
     <div className='MovieCard'>
       <div className='movie-image'>
-        <img src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title} />
+        <img src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title} aria-label="ver detalhes" />
       </div>
       <div className='movie-description'>
         <h3>{movie.title}</h3>
-        <Link to={`/movie/${movie.id}`} className='btn-details'>
-          Ver Detalhes
-        </Link>
+        <span>{formatarNumero(movie.vote_average)}/10 || {formatarDataBrasileira(movie.release_date)}</span>
       </div>
-    </div>
+    </div> </Link>
   );
 };
 
